6be5ac7a511b6a7fb378d9d676c30d818b0d99fc
akisame338 <akisame338@gmail.com>
2023-02-19 14:13:42 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -6,6 +6,18 @@ function execute() {
   eval ${cmd}
 }
 
+function make_backup() {
+  target=${1}
+  bakup_file="${target}.BAK"
+  echo "Make a backup copy as \`${bakup_file}\`."
+
+  if [ -f ${bakup_file} ]; then
+    echo "File \`${bakup_file}\` already exists!"
+    make_backup ${bakup_file}
+  fi
+  execute "mv ${target} ${bakup_file}"
+}
+
 function make_backup_or_delete() {
   target=${1}
 
@@ -13,10 +25,8 @@ function make_backup_or_delete() {
     echo "Symbolic link \`${target}\` exists, remove it."
     execute "rm ${target}"
   elif [ -f ${target} ]; then
-    bakup_file="${target}.BAK"
-    echo "File \`${target}\` already exists, make a backup copy \`${bakup_file}\` and remove."
-    execute "cp ${target} ${bakup_file}"
-    execute "rm ${target}"
+    echo "File \`${target}\` already exists!"
+    make_backup ${target}
   fi
 }
 
@@ -24,16 +34,20 @@ function create_symbolic_link() {
   src_file=${1}
   dst_file=${2}
 
+  echo "Try to create a symbolic link to \`${src_file}\` with the name \`${dst_file}\`"
   make_backup_or_delete ${dst_file}
   execute "ln -s ${src_file} ${dst_file}"
+  echo -e "Successfully created symbolic link.\n"
 }
 
-function copy() {
+function copy_file() {
   src_file=${1}
   dst_file=${2}
 
+  echo "Try to copy \`${src_file}\` to \`${dst_file}\`."
   make_backup_or_delete ${dst_file}
   execute "cp ${src_file} ${dst_file}"
+  echo -e "Successfully copied file.\n"
 }
 
 HOST_USER_DIR=`wslpath "$(wslvar USERPROFILE)"`
@@ -48,11 +62,11 @@ while read -d $'\0' src_file; do
 done < <(find ${SRC_DIR} -mindepth 1 -maxdepth 1 -print0)
 
 # WSL config
-copy "${PWD}/wsl/.wslconfig" "${HOST_USER_DIR}/.wslconfig"
+copy_file "${PWD}/wsl/.wslconfig" "${HOST_USER_DIR}/.wslconfig"
 
 # Windows Terminal config
 windows_terminal_dir=`find ${HOST_USER_DIR}/AppData/Local/Packages/ -type d -name 'Microsoft.WindowsTerminal_*' | head -n 1`
-copy "${PWD}/windows-terminal/settings.json" "${windows_terminal_dir}/LocalState/settings.json"
+copy_file "${PWD}/windows-terminal/settings.json" "${windows_terminal_dir}/LocalState/settings.json"
 
 # VSCode
 # user (Windows)
@@ -60,7 +74,7 @@ vscode_user_src_dir="${PWD}/vscode/user"
 vscode_user_dst_dir="${HOST_USER_DIR}/AppData/Roaming/Code/User"
 while read -d $'\0' src_file; do
   dst_file=`echo ${src_file} | sed "s@${vscode_user_src_dir}@${vscode_user_dst_dir}@"`
-  copy ${src_file} ${dst_file}
+  copy_file ${src_file} ${dst_file}
 done < <(find ${vscode_user_src_dir} -mindepth 1 -maxdepth 1 -print0)
 # remote (WSL2)
 vscode_remote_src_dir="${PWD}/vscode/remote"
